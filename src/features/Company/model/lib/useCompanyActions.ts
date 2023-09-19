efc48343fcd5bb6c8f01391698b8a0bd76bb82c9
Viktor Holik <1viktorholik@gmail.com>
2023-09-19 22:47:32 +0200

    Fix

--- src/features/Company/model/lib/useCompanyActions.ts
+++ src/features/Company/model/lib/useCompanyActions.ts
@@ -1,11 +1,9 @@
-import { validateNip } from '@/shared/lib/nip'
-import { useToast } from 'vue-toastification'
 import { useUpdateCompany } from '../services/useUpdateCompany'
 
 export function useCompanyActions() {
-    const toast = useToast()
     const { mutate } = useUpdateCompany()
 
+    // TODO: add debounce
     const onDataChange = (id: string, key: string) => (value: unknown) => {
         mutate({ [key]: value, id })
     }
