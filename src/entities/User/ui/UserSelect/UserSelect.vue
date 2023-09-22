a3e88511dda194f96655a1d6e5b85bc07bff720b
Viktor Holik <1viktorholik@gmail.com>
2023-09-22 23:11:58 +0200

    Fix

--- src/entities/User/ui/UserSelect/UserSelect.vue
+++ src/entities/User/ui/UserSelect/UserSelect.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import type { SelectOption } from '@/shared/ui/Select/types'
 import cls from './UserSelect.module.scss'
 import Select from '@/shared/ui/Select/Select.vue'
-import { computed, toRef, toRefs, watchEffect } from 'vue'
-import { useField, useForm } from 'vee-validate'
+import { toRefs } from 'vue'
+import { useField } from 'vee-validate'
 import { useOwners } from '../../model/services/useOwners'
 import type { User } from '../../model/types/user'
 
