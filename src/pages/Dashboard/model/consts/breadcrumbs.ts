8aad9bc9749e0673db0dc23e477abb4a077a7296
Viktor Holik <1viktorholik@gmail.com>
2023-11-26 21:11:44 +0100

    fix(changing main page breadcrumb)

--- src/pages/Dashboard/model/consts/breadcrumbs.ts
+++ src/pages/Dashboard/model/consts/breadcrumbs.ts
@@ -1,6 +1,6 @@
 import { type BreadcrumbsItem } from '@/shared/types/breadcrumbs'
 
 export const breadcrumbs: BreadcrumbsItem[] = [
-    { name: 'CIG Dashboard', path: '/dashboard' },
-    { name: 'Firmy', path: '/' },
+    { name: 'CIG Dashboard', path: '/' },
+    { name: 'Lista Leadów', path: '/dashboard' },
 ]
