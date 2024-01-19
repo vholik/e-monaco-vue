641f90cff9b3bb77246cb34dba09a0c63ba20d7a
DawidPolanski <dpolanski1999@gmail.com>
2024-01-19 16:16:45 +0100

    DatepickerRange some fixes

--- src/features/CompanyFilter/ui/DateFilter/DateFilter.vue
+++ src/features/CompanyFilter/ui/DateFilter/DateFilter.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 import FilterHeader from '@/shared/ui/FilterHeader/FilterHeader.vue'
-import { UserSelect } from '@/entities/User'
+import { ref, watch } from 'vue'
 import { useCompanyFilterStore } from '../../model/store/companyFilterStore'
 import { storeToRefs } from 'pinia'
 import DatepickerRange from '@/shared/ui/DatepickerRange/DatepickerRange.vue'
 
 const filterStore = useCompanyFilterStore()
-
 const { dateRange } = storeToRefs(filterStore)
 
+const localDateRange = ref(filterStore.dateRange)
+
 function onChangeFn(value: string[]) {
+    localDateRange.value = value
     filterStore.setDateRange(value)
 }
+
+function onClearDateRange() {
+    localDateRange.value = []
+    filterStore.setDateRange([])
+}
+
+watch(dateRange, (newValue) => {
+    localDateRange.value = newValue
+})
 </script>
 
 <template>
-    <FilterHeader :selected-count="dateRange.length">
+    <FilterHeader>
         <DatepickerRange
             name="date-select"
             multiple
-            :default-value="filterStore.dateRange"
+            :default-value="localDateRange"
             @update="onChangeFn"
+            @clear="onClearDateRange"
         />
     </FilterHeader>
 </template>
