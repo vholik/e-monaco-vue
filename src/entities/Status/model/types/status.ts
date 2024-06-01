25941025d500575001154ab1a827abce632981bb
DawidPolanski <dpolanski1999@gmail.com>
2024-06-01 14:48:19 +0200

    status update

--- src/entities/Status/model/types/status.ts
+++ src/entities/Status/model/types/status.ts
@@ -1,21 +1,21 @@
 export enum CompanyStatus {
     TO_BE_VERIFIED = 'to_be_verified', // DO WERYFIKACJI
     CALL_BACK = 'call_back', // ZADZWOŃ
-    UNREACHABLE = 'unreachable', // BRAK KONTAKTU
-    CONVERSATION = 'conversation', // ROZMOWY
-    AGREEMENT = 'agreement', // UMOWA
-    DOCUMENTS = 'documents', // DOKUMENTY
-    NEW_CLIENT = 'new_client', // NOWY KLIENT
-    OUR_CLIENT = 'our_client', // NASZ KLIENT
-    NEXT_CAMPAIGN = 'next_campaign', // NASTĘPNA KAMPANIA
+    DOESNT_EXIST = 'doesnt_exist', // NIE ISTNIEJE
     LEASE_ONLY = 'lease_only', // TYLKO LEASING
     NOT_A_TARGET = 'not_a_target', // NIE TARGET
-    NOT_INTERESTED = 'not_interested', // NIEZAINTERESOWANI
-    ACTIVITY = 'activity', // DZIAŁALNOŚĆ
-    RESIGNED = 'resigned', // ZREZYGNOWALI
-    OFFER = 'offer', // OFERTA
-    OTHER = 'other', // INNE
     LOCAL_PATRIOT = 'local_patriot', // LOKALNY PATRIOTA
-    CANNOT_BE_CALLED = 'cannot_be_called', // NIE ODBIERA
+    ACTIVITY = 'activity', // DZIAŁALNOŚĆ
+    NOT_INTERESTED = 'not_interested', // NIEZAINTERESOWANI
     SMALL_SAVINGS = 'small_savings', // MAŁA OSZCZĘDNOŚĆ
+    NEXT_CAMPAIGN = 'next_campaign', // NASTĘPNA KAMPANIA
+    CANNOT_BE_CALLED = 'cannot_be_called', // NIE ODBIERA
+    UNREACHABLE = 'unreachable', // BRAK KONTAKTU
+    OTHER = 'other', // INNY
+    CONVERSATION = 'conversation', // ROZMOWA
+    OFFER = 'offer', // OFERTA
+    AGREEMENT = 'agreement', // UMOWA
+    DOCUMENTS = 'documents', // DOKUMENTY
+    OUR_CLIENT = 'our_client', // NASZ KLIENT
+    RESIGNED = 'resigned', // ZREZYGNOWAŁ
 }
